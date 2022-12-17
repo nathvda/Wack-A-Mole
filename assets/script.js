667047a1f29ec836f4ab990ec5f8731219dc9423
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-12-17 22:05:23 +0100

    Adding a start game button

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,25 @@
 let molewrapper = document.getElementById("moles__wrapper");
+
+let element = document.createElement("button");
+let buttonText = document.createTextNode("New Game");
+
+element.appendChild(buttonText);
+element.setAttribute("id", "startGame");
+molewrapper.appendChild(element);
+
+element.addEventListener("click", (e) =>{
+    e.target.remove();
+    startGame();}
+    );
+
+
 let scoringArea = document.getElementById("score");
 
 let moles = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0 , 0 ,0 ,0 ,0 ,0 , 0];
 
 let score = 0;
 let clickCount = 0;
+
 
 function displayMole() {
     molewrapper.innerHTML = "";
@@ -30,23 +45,10 @@ function displayMole() {
 
 let difficulty = 1000;
 
-let displaying = setInterval(displayMole, difficulty);
+function startGame(){
+    let displaying = setInterval(displayMole, difficulty);
 
-molewrapper.addEventListener('click', (e) => {
-    if (e.target.classList.contains("mole") && clickCount == 0){
-        e.target.remove();
-        score += 100;
-        scoringArea.innerHTML = `Score : ${score} point${ (score == 1 || score == -1) ? "" : "s"}`;
-        clickCount = 1;
-    } else if (clickCount == 0) {
-        score -= 50;
-        scoringArea.innerHTML = `Score : ${score} point${ (score == 1 || score == -1) ? "" : "s"}`;
-        clickCount = 1;
-    } else if (clickCount !== 0){
-    }
-})
-
-let i = 60;
+    let i = 60;
 let endOfGame = setInterval(() => {
 i--; 
 document.getElementById("timer").innerHTML = `${i} seconde${(i > 1)? "s" : ""} restante${(i > 1)? "s" : ""}`;
@@ -58,6 +60,25 @@ if (i == 0){
 }
 
 },1000);
+
+setTimeout(() => {molewrapper.addEventListener('click', (e) => {
+    if (e.target.classList.contains("mole") && clickCount == 0){
+        e.target.remove();
+        score += 100;
+        scoringArea.innerHTML = `Score : ${score} point${ (score == 1 || score == -1) ? "" : "s"}`;
+        clickCount = 1;
+    } else if (clickCount == 0) {
+        score -= 50;
+        scoringArea.innerHTML = `Score : ${score} point${ (score == 1 || score == -1) ? "" : "s"}`;
+        clickCount = 1;
+    } else if (clickCount !== 0){
+    }
+})}, 1000 )
+
+
+}
+
+
 
 
 function endGame(){
